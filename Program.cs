f3453f117de08b9787b7642ff05eddec78dbbe1f
ephillips-tx <ephillips.tx@gmail.com>
2022-02-26 12:27:44 -0600

    update

--- Program.cs
+++ Program.cs
@@ -9,47 +9,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // new code
-var defaultConnectionString = string.Empty;
-
-if (builder.Environment.EnvironmentName == "Development")
-{
-    defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-}
-else
-{
-    // Use connection string provided at runtime by Heroku.
-    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
-    var userPassSide = connectionUrl.Split("@")[0];
-    var hostSide = connectionUrl.Split("@")[1];
-
-    var user = userPassSide.Split(":")[0];
-    var password = userPassSide.Split(":")[1];
-    var host = hostSide.Split("/")[0];
-    var database = hostSide.Split("/")[1].Split("?")[0];
-
-    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
-}
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(DataUtility.GetConnectionString(builder.Configuration));
 });
 
 builder.Services.AddScoped<IImageService, BasicImageService>();
-
 // end new code
 
 var app = builder.Build();
 
 //new code
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-//var dbContext = app.Services
-//                   .CreateScope().ServiceProvider
-//                   .GetRequiredService<ApplicationDbContext>();
-//await dbContext.Database.MigrateAsync();
 //end new code
 
 // Configure the HTTP request pipeline.
